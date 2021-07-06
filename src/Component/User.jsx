bdba5483793968bdfe39c1fafab11d29d746f6c6
Shawlycode <kutinaorlando@gmail.com>
2021-07-06 13:24:35 +0000

    added user component

---
+++ src/Component/User.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class User extends Component {
+  render() {
+    return (
+      <div>
+        <h1>name:"orlando</h1>
+        <p>Email:"orlandoshawly@gmail.com"</p>
+        <p>Gen:18</p>
+        <hr />
+      </div>
+    );
+  }
+}
+export default User;
