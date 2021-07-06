5e89fe0d7591af1ccf0b207ef66d0ee1d03b6c55
Shawlycode <kutinaorlando@gmail.com>
2021-07-06 14:07:52 +0000

    hr tag to component

--- src/Component/User.jsx
+++ src/Component/User.jsx
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 
-class User extends Component {
-  render() {
-    return (
-      <div>
-        <h1>name:"orlando</h1>
-        <p>Email:"orlandoshawly@gmail.com"</p>
-        <p>Gen:18</p>
-        <hr />
-      </div>
-    );
-  }
+const User =({user, index})=>{
+    return(
+        <div className="user" key={index}>
+            <h1>Name:{user.name}</h1>
+            <p>Email:{user.email}</p>
+            <p>Gen:{user.gen}</p>
+            <hr />
+        </div>
+
+    )
 }
+  
 export default User;
