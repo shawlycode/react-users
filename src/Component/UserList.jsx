afdbe1516877ae57b5448598d7618bfaac648136
Shawlycode <kutinaorlando@gmail.com>
2021-07-13 13:40:53 +0000

    created UserList component

---
+++ src/Component/UserList.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import User from './User';
+
+ class UserList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default UserList;
